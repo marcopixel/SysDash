07ec36248c9498ff415c55780605dcc0f5531a3f
Marco Vockner <marcopixel@live.de>
2017-12-23 20:38:58 +0100

    Adjusted skin sizes to be consistent for easier alignment

--- @Resources/scripts/GenerateDrives.lua
+++ @Resources/scripts/GenerateDrives.lua
@@ -9,7 +9,7 @@ function Initialize()
 							,'Drive=#Drive'..(i == 1 and '1' or i)..'#\n'
 							,'Total=1\n'
 							,'UpdateDivider=5\n'
-							,'Substitute="G":"GB",\n')
+							,'Substitute="G":"GB"\n')
 		  -- MeasureDriveFree
 		  File:write('[MeasureDriveFree'..(i == 1 and '1' or i)..']\n'
 						  ,'Measure=FreeDiskSpace\n'
@@ -36,24 +36,24 @@ function Initialize()
 		  -- MeterDriveLetter
 			File:write('[MeterDriveLetter'..(i == 1 and '1' or i)..']\n'
 							,'Meter=String\n'
-							,'MeterStyle=MeterDriveLetterStyle\n'
+							,'MeterStyle=StylePrimary\n'
 							,'X=(#Margin#*#Scale#)\n'
-							,'Y=(10*#Scale#)R\n'
+							,'Y=(17*#Scale#)R\n'
 							,'Text=#Drive'..(i == 1 and '1' or i)..'#\n'
 							,'LeftMouseDownAction=["#Drive'..(i == 1 and '1' or i)..'#"]\n')
 			-- MeterDriveValue
 			File:write('[MeterDriveValue'..(i == 1 and '1' or i)..']\n'
 							,'Meter=String\n'
-							,'MeterStyle=MeterDriveValueStyle\n'
+							,'MeterStyle=StyleSecondary | MeterDriveValueStyle\n'
 							,'MeasureName=MeasureDriveFree'..(i == 1 and '1' or i)..'\n'
 							,'MeasureName2=MeasureDriveTotal'..(i == 1 and '1' or i)..'\n'
 							,'X=((#Width#+#Margin#)*#Scale#)\n'
-							,'Y=(2*#Scale#)r\n')
+							,'Y=0r\n')
 			-- MeterDriveBar
 		  File:write('[MeterDriveBar'..(i == 1 and '1' or i)..']\n'
 						  ,'Meter=Shape\n'
 						  ,'X=(#Margin#*#Scale#)\n'
-						  ,'Y=(8*#Scale#)R\n'
+						  ,'Y=(-7*#Scale#)R\n'
 						  ,'Shape=Rectangle 0,0,(#Width#*#Scale#),(4*#Scale#),(2*#Scale#) | Fill Color #MainColor#,50 | StrokeWidth 0\n'
 						  ,'Shape2=Rectangle 0,0,((#Width#*#Scale#)*([MeasureDriveCalc'..(i == 1 and '1' or i)..':%]/100)),(4*#Scale#),(2*#Scale#) | Fill Color #MainColor#,245 | StrokeWidth 0\n'
 						  ,'DynamicVariables=1\n'
